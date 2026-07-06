703b97ecbc5fc4f8e1ad2b193ad8bd27a85cf988
mujii88 <mujtabakhan1036k@gmail.com>
2026-07-06 08:05:04 +0500

    july 06

--- 2026/07_July/july_06_2026.py
+++ 2026/07_July/july_06_2026.py
@@ -1,3 +1,5 @@
+# 1288. Remove Covered Intervals
+
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
         remaining=len(intervals)
@@ -9,6 +11,25 @@ class Solution:
                     if c<=a and b<=d:
                         remaining-=1
                         break
+        
+        return remaining
+        
+    
+
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        remaining=len(intervals)
+        seen=set()
+        for i in range(len(intervals)):
+            a,b=intervals[i]
+            for j in range(len(intervals)):
+                if j!=i:
+                    c,d=intervals[j]
+                    if c<=a and b<=d:
+                        if (a,b) not in seen:
+                            remaining-=1
+                            seen.add((a,b))
+                      
         
         return remaining
         
