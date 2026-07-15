e6618acc7b1b5c86ecd865160838177a46d5fd6c
mujii88 <mujtabakhan1036k@gmail.com>
2026-07-15 07:23:44 +0500

    july 15

---
+++ 2026/07_July/july_15_2026.py
@@ -0,0 +1,14 @@
+# 3658. GCD of Odd and Even Sums
+
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        curr=1
+        even=0
+        odd=0
+        while n>0:
+            odd+=curr
+            even+=curr+1
+            curr+=2
+            n-=1
+        ans=math.gcd(even,odd)
+        return ans
